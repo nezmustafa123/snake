b12ef385897c9dd7b21ab13c94f03ec9ab7eaf4c
nezmustafa123 <nezmustafa123@gmail.com>
2020-03-06 19:18:44 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const cvs = document.getElementById('canvas');
+
+const ctx = cvs.getContent('2d');
+
+let imageName = new Image();
+imageName.src = "path/img.png";
+
+let audioName = new Audio();
+audioName.src = "path/audio.png";
+
+audioName.play();
